5873cc2e86227e3fb43a070520b9a260bc1f936c
Brinzkii <dmbezehertny@gmail.com>
2023-09-19 16:32:40 -0400

    add full name to customer class

--- db.js
+++ db.js
@@ -1,8 +1,22 @@
 /** Database for lunchly */
 
-const pg = require("pg");
+const { Client } = require('pg');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const db = new pg.Client("postgresql:///lunchly");
+if (process.env.NODE_ENV === 'test') {
+	process.env.DATABASE = 'lunchly_test';
+} else {
+	process.env.DATABASE = 'lunchly';
+}
+
+let db = new Client({
+	user: process.env.USER,
+	host: process.env.HOST,
+	database: process.env.DATABASE,
+	password: process.env.PASSWORD,
+	port: process.env.PORT,
+});
 
 db.connect();
 
